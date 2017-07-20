68386d13b7b3fe2f6b20f69bd30bbbc89b15494b
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-07-20 21:16:23 +0700

    Add list log service

--- routes/logs.js
+++ routes/logs.js
@@ -3,28 +3,26 @@ const router = express.Router();
 const LogService = require('../service/LogService');
 
 router.get('/', function(req, res, next) {
-
-    kitty.save(function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('meow');
-        }
+  LogService.listLog()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      res.send(err);
     });
-    res.send('respond with a resource');
 });
 
 router.post('/', function(req, res, next) {
-    let log = {
-        'added_at': new Date()
-    };
-    LogService.addLog(log)
-        .then(result => {
-            res.send('Success : ' + log.added_at);
-        })
-        .catch(err => {
-            res.send('Error:' + err.message);
-        })
+  let log = {
+    'added_at': new Date()
+  };
+  LogService.addLog(log)
+    .then(result => {
+      res.send('Success : ' + log.added_at);
+    })
+    .catch(err => {
+      res.send('Error:' + err.message);
+    })
 });
 
 module.exports = router;
--- service/LogService.js
+++ service/LogService.js
@@ -4,22 +4,35 @@ const LogModel = require('../model/LogModel');
 const mongoose = require('../lib/db');
 
 const LogService = () => {
-    const addLog = data => {
-        return new Promise((resolve, reject) => {
-            let dt = new LogModel(data);
-            dt.save()
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(err => {
-                    reject(err);
-                });
+  const addLog = data => {
+    return new Promise((resolve, reject) => {
+      let dt = new LogModel(data);
+      dt.save()
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
         });
-    };
+    });
+  };
 
-    return {
-        addLog: addLog
-    }
+  const listLog = () => {
+    return new Promise((resolve, reject) => {
+      LogModel.find({})
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+
+  return {
+    addLog: addLog,
+    listLog: listLog
+  }
 };
 
 module.exports = LogService();
