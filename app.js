8c07f1f960c54f4a932365decbae1f3d76a37575
Yusuf Aji Wibowo <yusufaw@users.noreply.github.com>
2022-06-26 01:18:10 +0700

    Remove mimiku (#24)
    * Update example.env
    
    * Update databse name key
    
    * Remove logs related
    
    Co-authored-by: Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var bodyParser = require('body-parser');
 require('dotenv').config();
 
 var index = require('./routes/index');
-var logs = require('./routes/logs');
 
-const LogService = require('./service/LogService');
 const CommandService = require('./service/CommandService');
 
 var app = express();
@@ -29,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/logs', logs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- model/LogModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('../lib/db');
-
-module.exports = mongoose.model('Log', {
-    added_at: Date
-});
