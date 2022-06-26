a17e63c2cfb943c6708cbaa149690411e6d5f038
Yusuf Aji Wibowo <yusufaw@users.noreply.github.com>
2022-06-26 15:12:05 +0700

    Remove unused views (#30)
    * Remove serve-favicon
    
    * Remove body-parser
    
    * Remove morgan
    
    * Remove views
    
    * Remove cookie-parser
    
    * Remove cron
    
    * Remove debug logger
    
    * Remove pug
    
    * Remove static
    
    Co-authored-by: Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 require('dotenv').config();
 
 var index = require('./routes/index');
@@ -11,20 +7,6 @@ var index = require('./routes/index');
 const CommandService = require('./service/CommandService');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
